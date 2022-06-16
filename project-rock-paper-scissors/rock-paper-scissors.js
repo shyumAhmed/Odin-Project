127bce8ac7718fad56fba0ff24443fb4f67a3018
shyumAhmed <shyumahmed@gmail.com>
2022-06-15 20:09:42 -0400

    added button and div to make it more interactive. the div displays the the player cliked button

--- project-rock-paper-scissors/rock-paper-scissors.js
+++ project-rock-paper-scissors/rock-paper-scissors.js
@@ -1,24 +1,32 @@
+const content = document.querySelector(".content");
+const div = document.createElement("div");
+content.appendChild(div);
+let clicked = "";
+let compTally = 0;
+let playerTally = 0;
+let round = 1;
+
 function computerPlay() {
   const hand = ["rock", "paper", "scissors"];
   return hand[Math.floor(Math.random() * hand.length)];
 }
 
-function playerSelection() {
-  const validAnswer = false;
+function playerSelection(id) {
+  div.textContent = id;
+  clicked = id;
+  update();
+}
 
-  while (validAnswer == false) {
-    const usrInput = prompt("Type Rock, Paper or Scissors");
-    if (
-      usrInput.toLowerCase() != "rock" &&
-      usrInput.toLowerCase() != "paper" &&
-      usrInput.toLowerCase() != "scissors"
-    ) {
-      alert("Please fix your spelling and only enter rock, paper, or scissors");
-    } else {
-      console.log("Player Input: " + usrInput);
-      return usrInput;
-    }
-  }
+function playerButtonClicked() {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      if (round <= 5) {
+        playerSelection(e.target.id);
+        round++;
+      }
+    });
+  });
 }
 
 function results(compSel, playerSel) {
@@ -27,44 +35,34 @@ function results(compSel, playerSel) {
     (compSel == "paper" && playerSel == "rock") ||
     (compSel == "scissors" && playerSel == "paper")
   ) {
+    compTally++;
     return "Computer wins this round!";
   } else if (compSel == playerSel) {
     return "There is a tie";
   } else {
+    playerTally++;
     return "Player wins this round!";
   }
 }
 
-function game() {
-  let compTally = 0;
-  let playerTally = 0;
+function update() {
+  const comp = computerPlay();
+  console.log("Computer Input: " + comp);
+  console.log("player input: " + clicked);
 
-  for (let i = 0; i < 5; i++) {
-    console.log("Round: " + (i + 1));
-    const comp = computerPlay();
-    console.log("Computer Input: " + comp);
-    const res = results(comp, playerSelection());
-    console.log(res);
-    console.log("");
-
-    if (res == "Computer wins this round!") {
-      compTally++;
-      //console.log("COMPUTER wins: " + compTally);
-    }
-    if (res == "Player wins this round!") {
-      playerTally++;
-      //console.log("PLAYER wins: " + playerTally);
-    }
-  }
+  const res = results(comp, clicked);
+  console.log(res);
 
   console.log("");
-  if (compTally > playerTally) {
-    console.log("Computer is the Game Winner!");
-  } else if (compTally < playerTally) {
-    console.log("Player is the Game Winner!");
-  } else {
-    console.log("There is a tie");
+  if (round >= 5) {
+    if (compTally > playerTally) {
+      console.log("Computer is the Game Winner!");
+    } else if (compTally < playerTally) {
+      console.log("Player is the Game Winner!");
+    } else {
+      console.log("The game is tied ");
+    }
   }
 }
 
-game();
+playerButtonClicked();
